fed803a64da30073f3bedc165dd30f4e52997438
Margarita <ivanova5619@abv.bg>
2018-07-12 23:03:40 +0100

    set back mouseleave event

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ sections.forEach(function(section){
         //this.lastElementChild.style.padding = "45px 30px";
         this.lastElementChild.style.padding = "45px 30px 45px 20%";
     });
-    section.addEventListener("click", function close() {
+    section.addEventListener("mouseleave", function close() {
       this.lastElementChild.style.transition = "all 0.4s ease-in-out";
       this.lastElementChild.style.height = "0";
       this.lastElementChild.style.padding = "0";
