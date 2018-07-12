c7662c36ca9243d7cfc111cdbad95229ca4d6e0d
Margarita <ivanova5619@abv.bg>
2018-07-13 00:28:45 +0100

    removed close function

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ sections.forEach(function(section){
         //this.lastElementChild.style.padding = "45px 30px";
         this.lastElementChild.style.padding = "45px 30px 45px 20%";
     });
-    section.addEventListener("mouseleave", function close() {
-      this.lastElementChild.style.transition = "all 0.4s ease-in-out";
-      this.lastElementChild.style.height = "0";
-      this.lastElementChild.style.padding = "0";
-    });
+    // section.addEventListener("mouseleave", function close() {
+    //   this.lastElementChild.style.transition = "all 0.4s ease-in-out";
+    //   this.lastElementChild.style.height = "0";
+    //   this.lastElementChild.style.padding = "0";
+    // });
 });
