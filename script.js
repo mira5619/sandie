0c065dc691805b48427a40ed692bf3d14527916b
Margarita <ivanova5619@abv.bg>
2018-07-12 22:10:34 +0100

    added media quiry

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var sections = document.querySelectorAll('section');
 console.log(sections);
 sections.forEach(function(section){
     section.addEventListener("mouseenter", function expand(){
-        this.lastElementChild.style.transition = "all .8s linear";
+        this.lastElementChild.style.transition = "all .6s ease-in-out";
         this.lastElementChild.style.height = "100%";
-        this.lastElementChild.style.padding = "45px 30px";
-        
+        //this.lastElementChild.style.padding = "45px 30px";
+        this.lastElementChild.style.padding = "45px 30px 45px 20%";
     });
     section.addEventListener("mouseleave", function close() {
-      this.lastElementChild.style.transition = "all 0.4s linear";
+      this.lastElementChild.style.transition = "all 0.4s ease-in-out";
       this.lastElementChild.style.height = "0";
       this.lastElementChild.style.padding = "0";
     });
